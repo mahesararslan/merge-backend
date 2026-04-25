aacaba0c9649fa9a9bc38356ba6dff2f0dc916c7
Arslan Mahesar <mahesararslan1998@gmail.com>
2026-04-25 10:55:54 +0500

    Adds logo in notification

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -121,7 +121,7 @@ export class FirebaseService implements OnModuleInit {
           notification: {
             title: payload.title,
             body: payload.body,
-            icon: '/icon-192x192.png', // path to your app icon
+            icon: 'https://www.mergeedu.app/logo.svg', // path to your app icon
           },
           fcmOptions: {
             link: payload.data?.actionUrl || '/',
